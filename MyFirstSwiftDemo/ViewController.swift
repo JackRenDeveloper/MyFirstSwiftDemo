4d9099968c58c85f66e80d9288019ebe05c2e2a4
JackRenDeveloper <jackren_developer@163.com>
2016-05-30 10:25:32 -0700

    Initial Commit

---
+++ MyFirstSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstSwiftDemo
+//
+//  Created by JackRen on 16/5/30.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
